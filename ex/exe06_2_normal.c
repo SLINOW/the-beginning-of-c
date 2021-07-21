07e7d0e7668398fe9eb457138294b0568f89e1f5
karayuu <karayan0614@gmail.com>
2021-07-21 16:31:35 +0900

    [UPDATE] exe06_2_normal.c - add the normal solution

--- ex/exe06_2_normal.c
+++ ex/exe06_2_normal.c
@@ -1,144 +1,42 @@
 #include <stdio.h>
 
 /*
- * 1. まずは,日付が正しい形式であるかを確認する必要がある.
- *   - 月の範囲が1から12の間であることを確認する.
- *   - 日の範囲を確認する.
- *     - 4,6,9,11月は30日まで
- *     - 1,3,5,7,8,10,12月は31日まで
- *     - 2月は閏年なら29日まで,閏年でないなら28日まで
- *       - 年が400の倍数のとき閏年
- *       - 年が100の倍数のとき閏年でない
- *       - 年が4の倍数のとき閏年
- *   - 誕生日が未来だったら年齢の計算ができないのでダメ.
- * 2. 日付が正しい形式であることが確認できたら,年齢を求める.
+ * 本来,与えられた日付が正しいものであるかどうか,誕生日が未来ではないかを確認する必要がある.
+ * このよう正当性の確認を「バリデーション」という.
+ * 本プログラムは,初心者向けに「正しい日付形式が入力されていることを前提として」プログラムを作成する.
+ * (バリデーションを含めた詳しいプログラムが知りたい場合は, exe06_2_advanced.c を参照すること.
+ *
+ * 誕生日と与えられた任意の日付から,年齢を求める.
  *   - 年齢は,その年の誕生日をすぎていたら,年の引き算
  *   - また誕生日を迎えていなければ,年の引き算 - 1 歳
- *
- * 以上を把握した上でコードを書いていく.
  */
 
 int main() {
   int birth_year = 2001, birth_month = 6, birth_day = 14;
-  int today_year = 2020, today_month = 7, today_day = 20;
+  int today_year = 2021, today_month = 7, today_day = 21;
 
-  /* 入力バリデーション */
-  /* 月の範囲が1~12であることを確認する */
-  if (!(1 <= birth_month && birth_month <= 12)) {
-    printf("誕生日月が不正な値です.");
-    /* return 0 と書くことでプログラムを終了することが出来る */
-    return 0;
-  }
-  if (!(1 <= today_year && today_year <= 12)) {
-    printf("指定された月が不正です.");
-    return 0;
-  }
+  /* 月がすでにすぎていたら,誕生日をすぎていることを意味する. */
+  /*if (birth_month < today_month) {
+    *//* すでに誕生日が過ぎているパターン *//*
+  } else if (birth_month == today_month) {
+    if (birth_day <= today_day) {
+      *//* 誕生日が過ぎているパターン *//*
+    } else {
+      *//* 誕生日が過ぎていないパターン *//*
+    }
+  } else {
+    *//* 誕生日が過ぎていないパターン *//*
+  }*/
 
-  /* 1~12以外の値は弾いているので,ここで1~12が入力されていることが保証されている */
-  switch (birth_month) {
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-      /* 4,6,9,11月は30日まで */
-      if (!(1 <= birth_day && birth_day <= 30)) {
-        printf("誕生日に不正な日にちが指定されています.(%d年%d月は30日までです.)", birth_year, birth_month);
-        return 0;
-      }
-      break;
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-      /* 1,3,5,7,8,10,12月は31日まで */
-      if (!(1 <= birth_day && birth_day <= 31)) {
-        printf("誕生日に不正な日にちが指定されています.(%d年%d月は31日までです.)", birth_year, birth_month);
-        return 0;
-      }
-      break;
-    default:
-      /* 2月 */
-      if (birth_year % 400 == 0) {
-        /* 閏年の場合 */
-        if (!(1 <= birth_day && birth_day <= 29)) {
-          printf("誕生日に不正な日にちが指定されています.(%d年%d月は29日までです.)", birth_year, birth_month);
-          return 0;
-        }
-      } else if (birth_year % 100 == 0) {
-        /* 閏年でない場合 */
-        if (!(1 <= birth_day && birth_day <= 28)) {
-          printf("誕生日に不正な日にちが指定されています.(%d年%d月は28日までです.)", birth_year, birth_month);
-          return 0;
-        }
-      } else if (birth_year % 4 == 0) {
-        /* 閏年の場合 */
-        if (!(1 <= birth_day && birth_day <= 29)) {
-          printf("誕生日に不正な日にちが指定されています.(%d年%d月は29日までです.)", birth_year, birth_month);
-          return 0;
-        }
-      } else {
-        if (!(1 <= birth_day && birth_day <= 28)) {
-          printf("誕生日に不正な日にちが指定されています.(%d年%d月は28日までです.)", birth_year, birth_month);
-          return 0;
-        }
-      }
-      break;
-  }
-
-  /* 今日の日にちのバリデーション */
-  switch (today_month) {
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-      /* 4,6,9,11月は30日まで */
-      if (!(1 <= today_day && today_day <= 30)) {
-        printf("指定された日付に不正な日にちが指定されています.(%d年%d月は30日までです.)", today_year, today_month);
-        return 0;
-      }
-      break;
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-      /* 1,3,5,7,8,10,12月は31日まで */
-      if (!(1 <= today_day && today_day <= 31)) {
-        printf("指定された日付に不正な日にちが指定されています.(%d年%d月は31日までです.)", today_year, today_month);
-        return 0;
-      }
-      break;
-    default:
-      /* 2月 */
-      if (today_year % 400 == 0) {
-        /* 閏年の場合 */
-        if (!(1 <= today_day && today_day <= 29)) {
-          printf("指定された日付に不正な日にちが指定されています.(%d年%d月は29日までです.)", today_year, today_month);
-          return 0;
-        }
-      } else if (today_year % 100 == 0) {
-        /* 閏年でない場合 */
-        if (!(1 <= today_day && today_day <= 28)) {
-          printf("指定された日付に不正な日にちが指定されています.(%d年%d月は28日までです.)", today_year, today_month);
-          return 0;
-        }
-      } else if (today_year % 4 == 0) {
-        /* 閏年の場合 */
-        if (!(1 <= today_day && today_day <= 29)) {
-          printf("指定された日付に不正な日にちが指定されています.(%d年%d月は29日までです.)", today_year, today_month);
-          return 0;
-        }
-      } else {
-        if (!(1 <= today_day && today_day <= 28)) {
-          printf("指定された日付に不正な日にちが指定されています.(%d年%d月は28日までです.)", today_year, today_month);
-          return 0;
-        }
-      }
-      break;
+  /*
+   * 上記の処理を書きたいわけであるが,複数回同じことを書かなくてはならず効率的ではない.
+   * なので,論理演算子(||, &&)を用いて少し簡単に書いてみる.(もちろん上のコードでも動作する.)
+   */
+  if ((birth_month < today_month) || (birth_month == today_month && birth_day <= today_day)) {
+    /* 誕生日が過ぎているパターン */
+    printf("あなたの年齢は%d歳!", today_year - birth_year);
+  } else {
+    /* 誕生日が過ぎていないパターン */
+    printf("あなたの年齢は%d歳!", today_year - birth_year - 1);
   }
 }
