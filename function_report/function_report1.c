cdf66fdaf98424a959609f925d6832c4ef155ab0
karayuu <karayan0614@gmail.com>
2021-11-26 23:51:07 +0900

    [ADD] add function_report

---
+++ function_report/function_report1.c
@@ -0,0 +1,37 @@
+/*
+ * Created by karayuu on 2021/11/18.
+ */
+#include <stdio.h>
+
+int calc_sequence(int arg1, int arg2, int arg3) {
+  int xn = arg2;
+  int i;
+
+  /* 引数のバリデーション */
+  if (arg1 < 1 || arg2 < 1 || arg3 < 1) {
+    printf("Error : 引数が1以上の整数値ではない.\n");
+    return -1;
+  }
+
+  /* arg1 を足すのは (arg3 - 1)回 */
+  for (i = 1; i <= arg3 - 1; i++) {
+    xn += arg1;
+  }
+  return xn;
+}
+
+int main() {
+  int arg1 = 5, arg2 = 3, arg3= 8;
+
+  /* 関数の引数として与える値を出力 */
+  printf("-----------------\n");
+  printf("arg1 : %d\n", arg1);
+  printf("arg2 : %d\n", arg2);
+  printf("arg3 : %d\n", arg3);
+  printf("-----------------\n");
+
+  int result = calc_sequence(arg1, arg2, arg3);
+  if (result != -1) {
+    printf("x%d : %d", arg3, result);
+  }
+}
---
+++ function_report/function_report2.c
@@ -0,0 +1,50 @@
+/*
+ * Created by karayuu on 2021/11/18.
+ */
+#include <stdio.h>
+
+void calc(float x, float y, char op) {
+  int error = 0;
+  float answer;
+
+  if (!(0 <= x && x <= 500) || !(0 <= y && y <= 500)) {
+    printf("Error : 数値が0以上500以下ではない.\n");
+    printf("数値1 : %f\n", x);
+    printf("数値2 : %f\n", y);
+    error = -1;
+  }
+  if (!(op == '+' || op == '-' || op == '*' || op == '/')) {
+    printf("Error : 演算子が '+', '-', '*', '/' のいずれでもない.\n");
+    printf("演算子 : %c", op);
+    error = -1;
+  }
+  if (op == '/' && y == 0) {
+    printf("Error : 計算がゼロ除算となる.\n");
+    printf("%f ÷ %f\n", x, y);
+    error = -1;
+  }
+  if (error == -1) {
+    return;
+  }
+
+  switch (op) {
+    case '+':
+      answer = x + y;
+      break;
+    case '-':
+      answer = x - y;
+      break;
+    case '*':
+      answer = x * y;
+      break;
+    case '/':
+      answer = x / y;
+      break;
+  }
+
+  printf("%f %c %f = %f\n", x, op, y, answer);
+}
+
+int main() {
+  calc(1.5, 2.5, '+');
+}
